a38fd38fd4e1061584ca29be802288495a62b60b
Mathusan Selvarajah <mathusan@google.com>
2024-12-12 16:46:16 +0000

    init

---
+++ app-one/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans) bg-green-600">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div>APP ONE - Monorepo setup</div>
+        <div>TEST ENV: "{process.env.TEST_ENV}"</div>
+      </main>
+    </div>
+  );
+}
